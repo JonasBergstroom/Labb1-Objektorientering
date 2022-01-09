3f81dc1dc1c77a7379a4bdbd6b9d05bc6d8ea174
Jonas Bergström <71319570+JonasBergstroom@users.noreply.github.com>
2022-01-09 18:25:34 +0100

    Add files via upload
    All the classes and shapes

---
+++ Labb1_objektorientering/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Labb1_objektorientering
+
+{
+    public class Circle : Shape
+    {
+        public double radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double GetArea()
+        {
+            double area = Math.PI * (radius * radius);
+            return area;
+        }
+
+        public override double GetPerimeter()
+        {
+            double perimeter = (2 * Math.PI) * radius;
+            return perimeter;
+        }
+
+        public override string ToString()
+        {
+            return $"This is a {this.GetType().Name.ToLower()} with radius {radius}. The area is {GetArea()} and the perimeter is {GetPerimeter()}.";
+        }
+
+       
+    }
+}
---
+++ Labb1_objektorientering/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Labb1_objektorientering
+
+{
+    public class Rectangle : Shape
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+
+        public Rectangle(double width, double height) : this(width)
+        {
+            this.height = height;
+
+        }
+
+        public Rectangle(double width)
+        {
+            this.width = width;
+        }
+
+        public override double GetArea()
+        {
+            double area = 0;
+
+            if (height > 0)
+            {
+                area = width * height;
+
+            }
+            else
+            {
+                area = width * width;
+
+            }
+            return area;
+        }
+
+        public override double GetPerimeter()
+        {
+            double perimeter = 0;
+
+            if (height > 0)
+            {
+                perimeter = (width * 2) + (height * 2);
+
+            }
+            else
+            {
+                perimeter = width * 4;
+
+            }
+
+            return perimeter;
+        }
+
+        public override string ToString()
+        {
+            return $"This is a {this.GetType().Name.ToLower()} with height {(height == 0 ? width : height)} and width {width}. The area is {GetArea()} and the perimeter is {GetPerimeter()}.";
+        }
+
+    }
+}
